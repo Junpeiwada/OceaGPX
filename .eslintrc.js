8ece72bf083513b50e3369d7fa10ce44e86f8d30
Junpei Wada <junpeiwada@gmail.com>
2025-12-30 08:56:29 +0900

    chore: ESLint/Prettier設定とビルドスクリプト追加

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': 'warn',
+  },
+  overrides: [
+    {
+      files: ['src/main/**/*'],
+      env: {
+        browser: false,
+        node: true,
+      },
+    },
+    {
+      files: ['src/renderer/**/*'],
+      env: {
+        browser: true,
+        node: false,
+      },
+    },
+  ],
+};
